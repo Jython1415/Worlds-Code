a547701a1c54977236386af19876d1218684edb5
Jython1415 <36921818+Jython1415@users.noreply.github.com>
2018-03-01 16:24:32 +0900

    Update Arm_Control.c

--- Arm_Control.c
+++ Arm_Control.c
@@ -2,5 +2,14 @@ bool armCtrl_run = false;
 bool armCtrl_done = false;
 
 task armCtrl() {
-
+  while (armCtrl_run == true && armCtrl_done == false) {
+    //whatever works,
+        //might need a setting to keep arm in place
+          //so for that when you use like
+          //arm(1000,0); for normal control and
+          //arm(1000,1); for it to not stop running until the main program tells it to
+          //by setting armCtrl_run = false; In that mode it would just continue to run so the arm is held at that position.
+    
+    wait1Msec(25);  
+  }
 }
