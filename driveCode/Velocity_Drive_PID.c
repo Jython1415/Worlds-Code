58c62caaaaa03e06973524a0d99d03a7f1dd2a10
Jython1415 <36921818+Jython1415@users.noreply.github.com>
2018-03-01 16:18:04 +0900

    Update Velocity_Drive_PID.c

--- driveCode/Velocity_Drive_PID.c
+++ driveCode/Velocity_Drive_PID.c
@@ -2,7 +2,7 @@ bool velDrive_run = false;
 bool velDrive_done = false;
 
 task velDrivePID() {
-	while (velDrive_run == true && velDrive_done == false) {
+		while (velDrive_run == true && velDrive_done == false) {
 		//I'm probably going to do this.
 		
 		wait1Msec(25);
