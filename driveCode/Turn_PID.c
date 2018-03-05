1ae26b2762fed034da73e9aa8cedb1ddddbe511b
Jython1415 <36921818+Jython1415@users.noreply.github.com>
2018-03-05 15:59:33 +0900

    Update Turn_PID.c

--- driveCode/Turn_PID.c
+++ driveCode/Turn_PID.c
@@ -4,6 +4,8 @@ bool turn_done = false;
 task turnPID() {
     while (turn_run == true && turn_done == false) {
         //please add pid
+            //doesn't even have to be pid.
+            //just fast
         //just has to be fast
         //plase no oscillation
         //MoE can be like, 10 degress
