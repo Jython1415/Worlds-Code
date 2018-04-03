415067ae290a470d134e6f8f39a42a33dd2637e9
syoshiki424 <36990947+syoshiki424@users.noreply.github.com>
2018-04-03 17:49:01 +0900

    Update Arm_Control.c

--- Arm_Control.c
+++ Arm_Control.c
@@ -4,32 +4,31 @@ float kp_arm = 0.24;
 float ki_arm = 0;
 float kd_arm = 0.16;
 float dT_arm;
-float error_arm;
-float preverror_arm;
+float armError;
+float armPreverror;
 float pwr_arm;
-float integral_arm = 0;
-float derivative_arm = 0;
-float target_arm;
+float armIntegral = 0;
+float armDerivative = 0;
+float armTarget;
 int armMode = 0;
 
 task armCtrl() {
     while (armCtrl_run == true && armCtrl_done == false) {
-        error_arm = target_arm - SensorValue[arm_pot];
-        derivative_arm = error_arm - preverror_arm;
-        preverror_arm = error_arm;
+        armError = armTarget - SensorValue[arm_pot];
+        armDerivative = armError - armPreverror;
+        armPreverror = armError;
         
-        if(integral_arm > /*too much*/){
+        if(armIntegral > /*too much*/){
             ki_arm = 0;
         }
         else{
-            integral_arm = integral_arm + error_arm;
+            armIntegral = armIntegral + armError;
         }
 
-        pwr_arm = (error_arm*kp_arm) + (integral_arm*ki_arm) + (derivative_arm*kd_arm);
+        pwr_arm = (armError*kp_arm) + (armIntegral*ki_arm) + (armDerivative*kd_arm);
         setArm(pwr_arm);
         wait1Msec(25);
-
-        if(abs(error_arm) < /*certain value*/ && armMode = 0){
+        if(abs(armError) < /*certain value*/ && armMode = 0){
             armCtrl_done = true;
         }
     }
@@ -41,7 +40,7 @@ task armCtrl() {
 
         arm_pwr = (armError*armKp) + (armIntegral*armKi) + (armDerivative*armKd);
 
-        if(arm_pot!=/*value*/){
+        if(armError!=0){
           setArm(arm_pwr);
         }
         else{
@@ -51,7 +50,7 @@ task armCtrl() {
 }
 
 void arm(float aim, int Mode){
-    target_arm = aim;
+    armTarget = aim;
     armMode = Mode;
     startTask(armCtrl);
     //arm(1000,0); for normal control and
