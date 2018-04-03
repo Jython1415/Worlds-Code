21a050b4b3cef0f17c668107503e83ecea89a7a6
syoshiki424 <36990947+syoshiki424@users.noreply.github.com>
2018-04-03 17:26:48 +0900

    Update Drive_PD.c

--- driveCode/Drive_PD.c
+++ driveCode/Drive_PD.c
@@ -3,18 +3,20 @@ bool posDrive_done = false;
 float kp_drive = 0.24;
 float ki_drive = 0;
 float kd_drive = 0.16;
+float kp_gyro = 0;
 float dT_drive;
 float multiplier_driver; //temporarily stores the number that derivative_drive is multiplied by
 unsigned long lastTime;
 float error_drive;
 float preverror_drive;
-float pwr_drive;
 float integral_drive = 0;
 float derivative_drive = 0;
+float error_gyro;
 float target_drive;
-int driveMode = 0;
+float pwr_drive;
 
 task posDrivePD() {
+    SensorValue[gyro] = 0;
     while (posDrive_run == true && posDrive_done == false) {
         error_drive = target_drive - SensorValue[];
         derivative_drive = error_drive - preverror_drive;
@@ -25,18 +27,40 @@ task posDrivePD() {
         else{
             integral_drive = integral_drive + error_drive;
         }
-        
+        error_gyro = 0 + SensorValue[gyro];
         dT_drive = nPgmTime - lastTime; // Calculating time since last execution
         multiplier_driver = 25.0/dT_drive; // So that we find our actual rate of change.
-        pwr_arm = (error_drive*kp_drive) + (integral_drive*ki_drive) + (derivative_drive*kd_drive*multiplier_driver);
+        pwr_drive = (error_drive*kp_drive) + (integral_drive*ki_drive) + (derivative_drive*kd_drive*multiplier_driver);
         // made it so derivative term is also multiplied by multiplier
-        setArm(pwr_drive);
-        lastTime = nPgmTime; // I moved last time here so that I records the time when power was last set
         
-        if(abs(error_drive) < /*certain value*/ && driveMode = 0){
+        if(error_gyro > 10){
+            if(abs(pwr_drive + (error_gyro*kp_gyro)) > 127){
+                setRight(pwr_drive - (error_gyro*kp_gyro));
+                setLeft(pwr_drive);
+            }
+            else if(abs(pwr_drive + (error_gyro*kp_gyro)) < 127){
+                setRight(pwr_drive);
+                serLeft(pwr_drive + (error_gyro*kp_gyro));
+            }
+        }
+        else if(error_gyro < -10){
+            if(abs(pwr_drive + (error_gyro*kp_gyro)) > 127){
+                setRight(pwr_drive);
+                setLeft(pwr_drive - (error_gyro*kp_gyro));
+            }
+            else if(abs(pwr_drive + (error_gyro*kp_gyro)) < 127){
+                setRight(pwr_drive + (error_gyro*kp_gyro));
+                serLeft(pwr_drive);
+            }
+        }
+        else{
+            setRight(pwr_drive);
+            setLeft(pwr_drive);
+        }
+        lastTime = nPgmTime; // I moved last time here so that I records the time when power was last set
+        if(abs(error_drive) < /*certain value*/){
             posDrive_done = true;
         }
-        
         wait1Msec(25);
     }
 }
